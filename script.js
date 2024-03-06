9d7517cc4a3836b5e43ce1064c91aecb4de9a19d
dollarp-code <husseindollarp@yahoo.com>
2024-03-06 14:40:15 +0100

    Added the player switch functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 // selecting elements
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0 = document.getElementById('score--0');
 const score1 = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,7 +16,9 @@ const btnHold = document.querySelector('.btn--hold');
 score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add('hidden');
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', () => {
@@ -30,8 +32,14 @@ btnRoll.addEventListener('click', () => {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
