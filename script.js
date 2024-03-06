f46a5e5dcf60d33f69c0a2d584ff2fc31af34e47
dollarp-code <husseindollarp@yahoo.com>
2024-03-06 14:11:50 +0100

    Added the dice rolling functionality

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 'use strict';
+// selecting elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const score0 = document.getElementById('score--0');
+const score1 = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btn = document.querySelectorAll('.btn');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// Starting conditions
+score0.textContent = 0;
+score1.textContent = 0;
+diceEl.classList.add('hidden');
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', () => {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore;
+  } else {
+    // switch to next player
+  }
+});
